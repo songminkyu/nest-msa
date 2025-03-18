2d3386d9913634c0adfd06b3c4b2a959ad9f5d23
hunjuly <hunjuly@gmail.com>
2025-03-18 17:28:38 +0000

    Refactor: remove commented TODO for clarity in AppLoggerService

--- src/libs/common/logger/app-logger.service.ts
+++ src/libs/common/logger/app-logger.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, LoggerService } from '@nestjs/common'
 import winston from 'winston'
 
-// TODO 이름 winston으로 변경?
 @Injectable()
 export class AppLoggerService implements LoggerService {
     constructor(private logger: winston.Logger) {}
