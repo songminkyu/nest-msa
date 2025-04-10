0f474d04e8a6d4ce2a9570287cc9a4c54d6fb659
hunjuly <hunjuly@gmail.com>
2025-04-10 21:15:32 +0000

    refactor: Remove createWatchRecords function and update tests to use createWatchRecord for consistency

--- src/apps/__tests__/watch-records.fixture.ts
+++ src/apps/__tests__/watch-records.fixture.ts
@@ -1,21 +1,4 @@
-import { DateUtil } from 'common'
-import { testObjectId } from 'testlib'
 import { CommonFixture, createCommonFixture } from './utils'
-import { createWatchRecord } from './common.fixture'
-
-export const createWatchRecords = async (fix: CommonFixture, overrides = {}) => {
-    const baseDate = new Date(1)
-
-    return Promise.all(
-        Array.from({ length: 10 }, async (_, index) =>
-            createWatchRecord(fix, {
-                movieId: testObjectId(index),
-                watchDate: DateUtil.addDays(baseDate, index),
-                ...overrides
-            })
-        )
-    )
-}
 
 export interface Fixture extends CommonFixture {
     teardown: () => Promise<void>
--- src/apps/__tests__/watch-records.spec.ts
+++ src/apps/__tests__/watch-records.spec.ts
@@ -1,9 +1,8 @@
 import { expect } from '@jest/globals'
 import { WatchRecordDto } from 'apps/cores'
-import { OrderDirection } from 'common'
 import { expectEqualUnsorted, testObjectId } from 'testlib'
-import { createWatchRecords, Fixture } from './watch-records.fixture'
-import { buildWatchRecordCreateDto } from './common.fixture'
+import { buildWatchRecordCreateDto, createWatchRecord } from './common.fixture'
+import { Fixture } from './watch-records.fixture'
 
 describe('WatchRecords', () => {
     let fix: Fixture
@@ -17,7 +16,7 @@ describe('WatchRecords', () => {
         await fix?.teardown()
     })
 
-    describe('createWatchRecords', () => {
+    describe('createWatchRecord', () => {
         it('관람 기록을 생성해야 한다', async () => {
             const { createDto, expectedDto } = buildWatchRecordCreateDto()
 
@@ -31,14 +30,18 @@ describe('WatchRecords', () => {
         const customerId = testObjectId(0xa1)
 
         beforeEach(async () => {
-            records = await createWatchRecords(fix, { customerId })
+            records = await Promise.all([
+                createWatchRecord(fix, { customerId }),
+                createWatchRecord(fix, { customerId }),
+                createWatchRecord(fix, { customerId }),
+                createWatchRecord(fix, { customerId })
+            ])
         })
 
         it('기본 페이지네이션 설정으로 관람 기록을 가져와야 한다', async () => {
             const { items, ...paginated } = await fix.watchRecordsClient.findWatchRecords({
                 customerId,
-                take: 100,
-                orderby: { name: 'watchDate', direction: OrderDirection.desc }
+                take: 100
             })
 
             expect(paginated).toEqual({ skip: 0, take: 100, total: records.length })
