059cffab1d75244f31adb91adc38750c46c98040
hunjuly <hunjuly@gmail.com>
2025-07-08 11:05:41 +0000

    refactor: uncomment and streamline reset-infra.sh script for clarity and execution

--- scripts/reset-infra.sh
+++ scripts/reset-infra.sh
@@ -3,32 +3,29 @@ set -xeuo pipefail
 . "$(dirname "$0")/common.cfg"
 . $TEST_ENV_FILE
 
-# docker_compose --profile infra down --volumes --timeout 0
+docker_compose --profile infra down --volumes --timeout 0
 
-# rm -rf $WORKSPACE_ROOT/_output
-# mkdir -p $WORKSPACE_ROOT/$LOG_DIRECTORY
-# mkdir -p $WORKSPACE_ROOT/$FILE_UPLOAD_DIRECTORY
+rm -rf $WORKSPACE_ROOT/_output
+mkdir -p $WORKSPACE_ROOT/$LOG_DIRECTORY
+mkdir -p $WORKSPACE_ROOT/$FILE_UPLOAD_DIRECTORY
 
-# docker_compose --profile infra up -d
+docker_compose --profile infra up -d
 
-# SETUP_CONTAINERS="mongo-key-generator mongo-setup redis-setup nats-setup"
+SETUP_CONTAINERS="mongo-key-generator mongo-setup redis-setup nats-setup"
 
-# for CONTAINER in $SETUP_CONTAINERS; do
-#     CONTAINER_NAME="${PROJECT_NAME}-${CONTAINER}"
+for CONTAINER in $SETUP_CONTAINERS; do
+    CONTAINER_NAME="${PROJECT_NAME}-${CONTAINER}"
 
-#     if [ "$(docker wait "$CONTAINER_NAME")" -ne 0 ]; then
-#         echo "Error: Container '$CONTAINER_NAME' failed."
-#         exit 1
-#     else
-#         docker rm -v "$CONTAINER_NAME"
-#     fi
-# done
+    if [ "$(docker wait "$CONTAINER_NAME")" -ne 0 ]; then
+        echo "Error: Container '$CONTAINER_NAME' failed."
+        exit 1
+    else
+        docker rm -v "$CONTAINER_NAME"
+    fi
+done
 
 # The file is too large to save, so it will be created.
 RES_PATH=$WORKSPACE_ROOT/src/apps/__tests__/helpers/res
 
 dd if=/dev/zero bs=49999999 count=1 | tr '\0' 'a' > $RES_PATH/large.txt
 dd if=/dev/zero bs=50000000 count=1 | tr '\0' 'a' > $RES_PATH/oversized.txt
-
-# yes "abc123" | head -c 49999999 > $RES_PATH/large.txt
-# yes "abc123" | head -c 50000000 > $RES_PATH/oversized.txt
